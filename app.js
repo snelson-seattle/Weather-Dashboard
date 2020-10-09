49f43f2d79660cd50e95284319e041bccdad069b
Scott Nelson <snelson.seattle@gmail.com>
2020-10-08 18:27:15 -0700

    Broke the page layout a bit, but made the weather cards display correctly

--- app.js
+++ app.js
@@ -26,37 +26,31 @@ $(document).ready(function() {
             savedCites.push(keyword.toUpperCase());
             console.log(savedCites);
             localStorage.setItem("Searched-Cities", JSON.stringify(savedCites));
-        }
-        
-        
-
-        
+        }  
 
         // Construct endpoint URL for current day forecast request
         let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + keyword + "&appid=" + apiKey;
       
         // Construct endpoint URL for UV index request
         let uvURL = "https://api.openweathermap.org/data/2.5/uvi?";
-        
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {         
-          let temperature = ((response.list[0].main.temp - 273.15) * 1.8) + 32;
-          let windSpeed = response.list[0].wind.speed * 2.237;
-          let today = response.list[0].dt_txt.slice(0, 10);
-
-          console.log(today);
-          
-          $.ajax({
-            url: uvURL + "lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&appid=" + apiKey,
+        $.ajax({
+            url: queryURL,
             method: "GET"
-            }).then(function(reply){
+          }).then(function(response) {   
+            console.log(response);      
+            let temperature = ((response.list[0].main.temp - 273.15) * 1.8) + 32;
+            let windSpeed = response.list[0].wind.speed * 2.237;
+            let today = response.list[0].dt_txt.slice(0, 10);
+            
+            $.ajax({
+              url: uvURL + "lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&appid=" + apiKey,
+              method: "GET"
+              }).then(function(reply) {
                 // Build Current Day Forecast UI                            
                 let currentForecast = $("#current-forecast");
                 currentForecast.empty(); // Clear the old info
-
+  
                 let uvIndex = reply;   
                 let cityEl = $("<h1>").text(keyword.toUpperCase());
                 currentForecast.append(cityEl);
@@ -70,39 +64,57 @@ $(document).ready(function() {
                 currentForecast.append(windEl);
                 let uvEl = $("<p>").text("UV Index: " + uvIndex.value);
                 currentForecast.append(uvEl);
-
+  
                 // Build 5 Day Forecast UI
-                let fiveDayForecasst = $("#five-day-forecast");
-                fiveDayForecasst.empty(); // CLear out the old info
+                let fiveDayForecast = $("#five-day-forecast");
+                fiveDayForecast.empty(); // CLear out the old info
                 let fiveDayHeading = $("<h3>").text("5-Day Forecast:");
-                fiveDayForecasst.append(fiveDayHeading);
-
+                fiveDayForecast.append(fiveDayHeading);
+                let row = $("<div>").addClass("row");
+                fiveDayForecast.append(row);
+  
                 for(let i = 5; i <= 37; i += 8){
+                    let column = $("<div>").addClass("column");
                     let weatherCard = $("<div>").addClass("card text-white bg-info");
                     let cardBody = $("<div>").addClass("card-body");
                     let cardTitle = $("<h5>").addClass("card-title").text(response.list[i].dt_txt.slice(0,10));
+                    let iconEl = $("<h1>");
+                    switch(response.list[i].weather[0].main){
+                        case "Clear":
+                            iconEl.addClass("fas fa-sun");
+                            break;
+                        case "Clouds":
+                            iconEl.addClass("fas fa-cloud");
+                            break;
+                        case "Rain":
+                            iconEl.addClass("fas fa-cloud-rain");
+                            break;
+
+                    }
                     let fTemp = ((response.list[i].main.temp - 273.15) * 1.8) + 32;
-                    let fiveTempEl = $("<p>").text("Temperature: " + fTemp.toFixed(1) + " F");
+                    let fiveTempEl = $("<p>").text("Temp: " + fTemp.toFixed(1) + " F");
                     let fiveHumidityEl = $("<p>").text("Humidity: " + response.list[i].main.humidity + "%");
-
-
+  
+  
                     cardBody.append(cardTitle);
+                    cardBody.append(iconEl);
                     cardBody.append(fiveTempEl);
                     cardBody.append(fiveHumidityEl);
-                    weatherCard.append(cardBody);                    
-                    fiveDayForecasst.append(weatherCard);
-                }
+                    weatherCard.append(cardBody);    
+                    column.append(weatherCard);                
+                    row.append(column);
+                 }
             });   
- 
+   
         });
 
-       
-    });
+    
+
+    });    
 
     function loadSavedCities () {
         if(JSON.parse(localStorage.getItem("Searched-Cities"))){
-            savedCites = JSON.parse(localStorage.getItem("Searched-Cities"));
-            console.log(savedCites);
+            savedCites = JSON.parse(localStorage.getItem("Searched-Cities"));            
         }    
         for(let i = 0; i < savedCites.length; i++){
             let savedCityList = $("#city-list");
@@ -114,17 +126,15 @@ $(document).ready(function() {
 
 });
 
-
-
-/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul> 
-</div> */
+// <div class="column">
+//     <div class="card">..</div>
+//   </div>
+//   <div class="column">
+//     <div class="card">..</div>
+//   </div>
+//   <div class="column">
+//     <div class="card">..</div>
+//   </div>
+//   <div class="column">
+//     <div class="card">..</div>
+//   </div>
